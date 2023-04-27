394e37f0d750238dd5e95ab2b6c7c8e641bd083f
giagara <giagara@yahoo.it>
2023-04-27 17:21:56 +0200

    fix: added phpunit and first test

---
+++ tests/Feature/CommandTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Giagara\AsanaWebhook\Tests\Feature;
+
+use Giagara\AsanaWebhook\Tests\TestCase;
+
+class CommandTest extends TestCase
+{
+
+    public function test_command_will_execute_correctyl()
+    {
+        $this->assertTrue(true);
+    }
+}
