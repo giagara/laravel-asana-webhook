ffdbabb03d866089b6c4f9314aa075a2c8bd08cb
giagara <giagara@yahoo.it>
2023-04-28 09:17:46 +0200

    chore: improve test fix: github action strategy

--- tests/Feature/CommandTest.php
+++ tests/Feature/CommandTest.php
@@ -3,12 +3,43 @@
 namespace Giagara\AsanaWebhook\Tests\Feature;
 
 use Giagara\AsanaWebhook\Tests\TestCase;
+use Illuminate\Support\Facades\Http;
 
 class CommandTest extends TestCase
 {
 
-    public function test_command_will_execute_correctyl()
+    /**
+     * @dataProvider httpProvider 
+     */
+    public function test_command_will_execute_correctyl(int $status, string $fixture, int $exit_code)
     {
-        $this->assertTrue(true);
+
+        config(['asana-webhook.personal_access_token' => 'test_value']);
+
+        Http::fake([
+            'https://app.asana.com/api/1.0/webhooks' => Http::response(
+                json_decode(file_get_contents(__DIR__ . "/../fixtures/{$fixture}.json"), true),
+                $status
+            )
+        ]);
+
+        $this->artisan('make:asana-webhook --resource=1 --target=2')->assertExitCode($exit_code);
+        
+    }
+
+    public function httpProvider() : array
+    {
+        return [
+            "ok" => [
+                "status" => 200,
+                "fixture" => "200",
+                "exit_code" => 0
+            ],
+            "error" => [
+                "status" => 400,
+                "fixture" => "400",
+                "exit_code" => 91
+            ],
+        ];
     }
 }
