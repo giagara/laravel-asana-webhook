51a6b0b52d315c8233e811fb4a89197f6ba9234f
giagara <giagara@yahoo.it>
2023-11-20 10:44:43 +0100

    wip

--- tests/Feature/HettpTest.php
+++ tests/Feature/HettpTest.php
@@ -5,9 +5,53 @@ namespace Giagara\AsanaWebhook\Tests\Feature;
 use Giagara\AsanaWebhook\Tests\Classes\FakeInvokableClass;
 use Giagara\AsanaWebhook\Tests\Classes\FakeInvokableErrorClass;
 use Giagara\AsanaWebhook\Tests\TestCase;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 
 class HttpTest extends TestCase
 {
+    public function test_controller_handle_hook_secret_correctly()
+    {
+
+        config(['asana-webhook.routes' => [
+            'webhook' => FakeInvokableClass::class,
+        ]]);
+
+        $secret = Str::random(10);
+
+        $this->reloadRoutes();
+
+        $response = $this->post(
+            'api/webhook',
+            json_decode(file_get_contents(__DIR__.'/../fixtures/payload.json'), true),
+            [
+                'X-Hook-Secret' => $secret,
+            ]
+        );
+
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+        $response->assertHeader('X-Hook-Secret', $secret);
+
+    }
+
+    public function test_controller_abort_if_header_not_set()
+    {
+
+        config(['asana-webhook.routes' => [
+            'webhook' => FakeInvokableClass::class,
+        ]]);
+
+        $this->reloadRoutes();
+
+        $response = $this->post(
+            'api/webhook',
+            json_decode(file_get_contents(__DIR__.'/../fixtures/payload.json'), true)
+        );
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+
+    }
+
     public function test_controller_handle_data_correctly()
     {
 
@@ -25,7 +69,7 @@ class HttpTest extends TestCase
             ]
         );
 
-        $response->assertStatus(204);
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
 
     }
 
@@ -46,7 +90,7 @@ class HttpTest extends TestCase
             ]
         );
 
-        $response->assertStatus(404);
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
 
     }
 
@@ -67,7 +111,7 @@ class HttpTest extends TestCase
             ]
         );
 
-        $response->assertStatus(500);
+        $response->assertStatus(Response::HTTP_INTERNAL_SERVER_ERROR);
 
     }
 }
